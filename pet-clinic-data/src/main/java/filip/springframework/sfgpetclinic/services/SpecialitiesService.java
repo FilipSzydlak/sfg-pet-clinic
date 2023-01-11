ebf5224c52ec17e3e9e0d9ce2379a530d0a65437
Filip Szydłak <266912@student.pwr.edu.pl>
2023-01-11 21:38:32 +0100

    SpecialitiesServiceMap Impl. Closes #33

---
+++ pet-clinic-data/src/main/java/filip/springframework/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package filip.springframework.sfgpetclinic.services;
+
+import filip.springframework.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality,Long> {
+}
