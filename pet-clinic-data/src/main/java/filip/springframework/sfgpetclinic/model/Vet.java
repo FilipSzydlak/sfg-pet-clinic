f3db995245ec478d3ff3df44d907301b8930f120
Filip Szydłak <266912@student.pwr.edu.pl>
2023-01-14 21:33:56 +0100

    updated vet to use JPA. Closes #39

--- pet-clinic-data/src/main/java/filip/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/filip/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,18 @@
 package filip.springframework.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name ="vet_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+                inverseJoinColumns = @JoinColumn(name = "speciality"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
